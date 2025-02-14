f915b4d66eb5df19718728c47a03af6fd3110a83
Carlos Oliveira <caduolivera17@gmail.com>
2025-02-14 12:19:44 -0300

    inicializando

---
+++ roomtal-backend/src/main/java/com/roomtal/roomtal_backend/RoomtalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.roomtal.roomtal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomtalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomtalBackendApplication.class, args);
+	}
+
+}
---
+++ roomtal-backend/src/test/java/com/roomtal/roomtal_backend/RoomtalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomtal.roomtal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomtalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
